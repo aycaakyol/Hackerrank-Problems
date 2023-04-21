65b6708f3f1bf3fe1479538a1b53e2746b605235
aycaakyol <aycaakyol3@gmail.com>
2023-04-21 23:38:08 +0300

    from leetcode

--- Anagram.py
+++ Anagram.py
@@ -1,16 +1,31 @@
 class Solution:
+    # def isAnagram(self, s: str, t: str) -> bool:
+    #     d1 = {}
+    #     d2 = {}
+
+    #     if len(s) == len(t):
+    #         for i in range(len(s)):
+    #             if s[i] in t:
+    #                 t = t.replace(s[i], "", 1)
+    #             else:
+    #                 return False
+
+    #         if len(t) == 0:
+    #             return True
+    #     else:
+    #         return False
     def isAnagram(self, s: str, t: str) -> bool:
-        d1 = {}
-        d2 = {}
+        d = [0] * 26
 
         if len(s) == len(t):
             for i in range(len(s)):
-                if s[i] in t:
-                    t = t.replace(s[i], "", 1)
-                else:
-                    return False
-
-            if len(t) == 0:
-                return True
+                    d[ord(s[i]) - ord('a')] += 1
+                    d[ord(t[i]) - ord('a')] -= 1
         else:
             return False
+
+        for i in d:
+            if i != 0:
+                return False
+
+        return True 
